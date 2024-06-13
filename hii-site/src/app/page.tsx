454e648cbc87ea9e08670d2a06919b413608c253
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2024-06-13 17:22:09 +0900

    トップページのアップデート
    コンポーネントの追加

--- hii-site/src/app/page.tsx
+++ hii-site/src/app/page.tsx
@@ -1,3 +1,15 @@
+import Profile from "@/component/top_page/about";
+import Skill from "@/component/top_page/skill";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="relative top-24">
+      <div>
+        <Profile></Profile>
+      </div>
+      <div className=" relative mt-40">
+        <Skill></Skill>
+      </div>
+    </main>
+  );
 }
