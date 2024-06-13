841eab301a08d12a9403d347c65ebc7f4f6bf6f2
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2024-06-13 17:19:36 +0900

    カスタムカラーの追加

--- hii-site/tailwind.config.ts
+++ hii-site/tailwind.config.ts
@@ -1,14 +1,16 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/component/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
+    colors: {
+      Customblack: "#282828",
+      Customgreen: "#124F53",
+      white: "#FFFFFF",
+    },
   },
   plugins: [],
 };
-export default config;
