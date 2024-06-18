32352101bedea1380ec7bcdcbeb869b5603fbd30
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2024-06-18 13:37:55 +0900

    記事一覧の追加

---
+++ hii-site/src/component/article/data/articles.ts
@@ -0,0 +1,23 @@
+export const articles = [
+  {
+    title: "情報系学生3年目の年にやること",
+    description:
+      "こんにちは、情報系専門学校に通っているひーです。本記事では、2024年のやることリストとその内容についてまとめます。",
+    platform: "Zenn",
+    url: "https://zenn.dev/aputech/articles/0d4f0d24f3e73b",
+  },
+  {
+    title: "チーム開発の難しさ。",
+    description:
+      "学内のイベントに出展する作品をチームで制作しています。その中で学んだチーム開発の難しさを雑に書き残しておきたいと思います。",
+    platform: "Zenn",
+    url: "https://zenn.dev/aputech/articles/d99e8c4b70efeb",
+  },
+  {
+    title: "TailwindCSSが反映されない！",
+    description:
+      "Next.js、TailwindCSSを勉強中、TailwindCSSが反映されない。という事になったので、原因と対応についてまとめたいと思います。",
+    platform: "Zenn",
+    url: "https://zenn.dev/aputech/articles/107984712771aa",
+  },
+];
