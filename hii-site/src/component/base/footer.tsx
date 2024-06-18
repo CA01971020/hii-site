488d0e9a8a55a6287a7992efb50dd32278a1cd08
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2024-06-18 11:38:44 +0900

    SNSリンクの追加

--- hii-site/src/component/base/footer.tsx
+++ hii-site/src/component/base/footer.tsx
@@ -1,8 +1,21 @@
 export default function CustomFooter() {
   return (
     <footer className="w-full h-10 text-base text-Customblack bg-white ">
-      <div className="cursor-default pt-2 pl-2">
-        <p className="copyright">&copy;2024 hii Site.</p>
+      <div className="cursor-default pt-2 flex">
+        <div className=" absolute left-3">
+          <p className="copyright">&copy;2024 hii Site.</p>
+        </div>
+        <div className=" absolute right-6 text-Customblue">
+          <a href="https://x.com/aputech2024" className=" mr-4 p-0.5">
+            Twitter
+          </a>
+          <a href="https://zenn.dev/aputech" className=" mr-4 p-0.5">
+            Zenn
+          </a>
+          <a href="https://note.com/hii_hgmt/" className="p-0.5">
+            note
+          </a>
+        </div>
       </div>
     </footer>
   );
