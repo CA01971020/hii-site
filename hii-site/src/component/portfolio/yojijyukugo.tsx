7dd7dc39b29de818f1d5e4bd476f97bac9d1c42d
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2024-09-12 11:22:16 +0900

    リファクタリング

---
+++ hii-site/src/component/portfolio/yojijyukugo.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export default function Yojijyukugo() {
+  return (
+    <div>
+      <a
+        href="https://yojijyukugo.vercel.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="relative mr-12 lg:mr-96 ml-12 lg:ml-96 rounded-xl p-10 bg-white grid grid-cols-2 lg:grid-cols-1">
+          <Image
+            alt="yojijyukugo"
+            src="/yojiicon.png"
+            width="70"
+            height="10"
+            className=" absolute left-8 lg:left-20 top-0.5"
+          ></Image>
+          <p className=" absolute w-40 top-7 right-8 lg:right-20 text-Customblue">
+            四字熟語を作るアプリ
+          </p>
+        </div>
+      </a>
+    </div>
+  );
+}
--- hii-site/src/component/top_page/portfolio.tsx
+++ hii-site/src/component/top_page/portfolio.tsx
@@ -1,28 +1,11 @@
-import Image from "next/image";
+import Yojijyukugo from "../portfolio/yojijyukugo";
 
 export default function Portfolio() {
   return (
     <section id="portfolio" className="relative pt-56 text-white text-center">
       <h1>制作物</h1>
       <div className="mt-48">
-        <a
-          href="https://yojijyukugo.vercel.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className="relative mr-12 lg:mr-96 ml-12 lg:ml-96 rounded-xl p-10 bg-white grid grid-cols-2 lg:grid-cols-1">
-            <Image
-              alt="yojijyukugo"
-              src="/yojiicon.png"
-              width="70"
-              height="10"
-              className=" absolute left-8 lg:left-20 top-0.5"
-            ></Image>
-            <p className=" absolute w-40 top-7 right-8 lg:right-20 text-Customblue">
-              四字熟語を作るアプリ
-            </p>
-          </div>
-        </a>
+        <Yojijyukugo />
       </div>
     </section>
   );
