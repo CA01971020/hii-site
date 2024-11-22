a5ce67ab319e00794bc209916d46d8d4e9434bb1
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2024-11-22 11:42:48 +0900

    ポートフォリオの追加

--- hii-site/src/component/top_page/portfolio.tsx
+++ hii-site/src/component/top_page/portfolio.tsx
@@ -1,11 +1,13 @@
 import Yojijyukugo from "../portfolio/yojijyukugo";
+import Realdaily from "../portfolio/realdaily";
 
 export default function Portfolio() {
   return (
     <section id="portfolio" className="relative pt-56 text-white text-center">
       <h1>制作物</h1>
-      <div className="mt-48">
+      <div className="mt-48 flex flex-col gap-20">
         <Yojijyukugo />
+        <Realdaily />
       </div>
     </section>
   );
