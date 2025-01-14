9e16dcca986c5672b04ae530903fe3a07211db6a
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-01-14 16:24:23 +0900

    スタイルの修正

--- hii-site/src/component/top_page/portfolio.tsx
+++ hii-site/src/component/top_page/portfolio.tsx
@@ -1,13 +1,15 @@
 import Yojijyukugo from "../portfolio/yojijyukugo";
 import Realdaily from "../portfolio/realdaily";
+import NoWrongAnswerQuizGame from "../portfolio/no-wrong-answer-quiz-game";
 
 export default function Portfolio() {
   return (
     <section id="portfolio" className="relative pt-56 text-white text-center">
       <h1>制作物</h1>
-      <div className="mt-48 flex flex-col gap-20">
+      <div className="mt-12 flex flex-col gap-20">
         <Yojijyukugo />
         <Realdaily />
+        <NoWrongAnswerQuizGame />
       </div>
     </section>
   );
