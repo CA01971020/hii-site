2e798e09db04d251315d6ba4861cb80e75b6ae86
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-02-20 15:35:08 +0900

    Create custombutton.tsx

---
+++ hii-site/src/component/ui/custombutton.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type CustomButtonProps = {
+  buttontext: string;
+};
+
+const CustomButton: React.FC<CustomButtonProps> = ({ buttontext }) => {
+  return (
+    <button className=" bg-Customblack pl-12 pr-12 pt-5 pb-5 rounded-full hover:bg-Customgray ring-1 ring-black/50">
+      {buttontext}
+    </button>
+  );
+};
+
+export default CustomButton;
